0d46daebacddb67690ae0aa487cff4daead088d1
dccif <heyujieboy@hotmail.com>
2023-03-02 11:32:10 -0500

    add test

--- javascript/day5.js
+++ javascript/day5.js
@@ -4,6 +4,7 @@ function fivechance() {
     chance += 1;
     if (chance <= 5) {
       console.log("Congrats you earn the chance!");
+      console.log("Test sentences");
     } else {
       console.log("Sorry you missed the chance");
     }
