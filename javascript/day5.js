0b813db1eb8b4c4bc0ca210cb89714070287236e
dccif <heyujieboy@hotmail.com>
2023-03-02 11:31:12 -0500

    add test sentences

--- javascript/day5.js
+++ javascript/day5.js
@@ -1,17 +1,18 @@
 function fivechance() {
-	let chance = 0
-	return function () {
-		chance += 1
-		if (chance <= 5) {
-			console.log("Congrats you earn the chance!")
-		} else {
-			console.log("Sorry you missed the chance")
-		}
-	}
+  let chance = 0;
+  return function () {
+    chance += 1;
+    if (chance <= 5) {
+      console.log("Congrats you earn the chance!");
+      console.log("Test the file and next");
+    } else {
+      console.log("Sorry you missed the chance");
+    }
+  };
 }
 
-function longerThan(minlength){
-	return (elem) =>{
-		return (elem.length > minlength)
-	}
+function longerThan(minlength) {
+  return (elem) => {
+    return elem.length > minlength;
+  };
 }
